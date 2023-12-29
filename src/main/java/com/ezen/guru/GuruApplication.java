1bdd92b1ec6966e05935b1ba4622291486d609d4
kwonnahyun <skgusskgus2@naver.com>
2023-12-29 16:05:50 +0900

    Add

---
+++ src/main/java/com/ezen/guru/GuruApplication.java
@@ -0,0 +1,13 @@
+package com.ezen.guru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ezen/guru/GuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezen.guru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
