4af83e4600be33125fc3b1f5f76419902305ba22
Park Juyeong <jypark0819@naver.com>
2024-01-02 16:22:42 +0900

    setting domains_final

---
+++ src/main/java/com/ezen/guru/domain/Company.java
@@ -0,0 +1,50 @@
+package com.ezen.guru.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "company")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Company {
+
+    @Id
+    @Column(name="company_id", updatable = false)
+    private String companyId;
+    @Column(name="material_id")
+    private int materialId;
+    @Column(name="company_name")
+    private String companyName;
+    @Column(name="ceo")
+    private String ceo;
+    @Column(name="tel")
+    private String tel;
+    @Column(name="email")
+    private String email;
+    @Column(name="address")
+    private String address;
+
+    @Builder
+    public Company(String companyId,
+                   int materialId,
+                   String companyName,
+                   String ceo,
+                   String tel,
+                   String email,
+                   String address) {
+        this.companyId = companyId;
+        this.materialId = materialId;
+        this.companyName = companyName;
+        this.ceo = ceo;
+        this.tel = tel;
+        this.email = email;
+        this.address = address;
+    }
+}
