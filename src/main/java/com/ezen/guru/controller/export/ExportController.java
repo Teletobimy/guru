ca4659b61616d805a70956a75fc5d6731f1f2ec2
kim hwijin <rlagnlwls0127@naver.com>
2024-01-05 10:20:00 +0900

    export @RestController -> @Controller modify

--- src/main/java/com/ezen/guru/controller/export/ExportController.java
+++ src/main/java/com/ezen/guru/controller/export/ExportController.java
@@ -3,14 +3,14 @@ package com.ezen.guru.controller.export;
 import com.ezen.guru.dto.plan.ProducePlanerDTO;
 import com.ezen.guru.service.export.ExportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/export")
 @RequiredArgsConstructor
 public class ExportController {
