9a0bf4121c1f2ddba7cc58bdffcf4f7451f099c4
kwonnahyun <rnjsskgus4244@gmail.com>
2024-01-08 09:02:56 +0900

    shipment page - conflict 해결

--- src/main/java/com/ezen/guru/repository/receive/shipment/ShipmentCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ezen.guru.repository.receive.shipment;
-
-import com.ezen.guru.domain.Shipment;
-
-import java.util.List;
-
-public interface ShipmentCustomRepository {
-List<Shipment> shipmentList(Shipment shipment);
-}
