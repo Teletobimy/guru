8c3b7e224cffcf0a294c240dfbe648cc737b1886
kwonnahyun <skgusskgus2@naver.com>
2024-01-17 11:20:56 +0900

    shipment detail page - 명세서 스타일 변경 - orderDatailRepository update query변경 - company entity 중복 키 설정 - shipmentList rangeDatePicker 필터 적용

--- src/main/java/com/ezen/guru/service/receive/ShipmentService.java
+++ src/main/java/com/ezen/guru/service/receive/ShipmentService.java
@@ -6,12 +6,13 @@ import com.ezen.guru.dto.receive.ShipmentDetailResponse;
 import com.ezen.guru.dto.receive.ShipmentResponse;
 import org.springframework.data.domain.Page;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface ShipmentService {
     //public Page<ShipmentResponse> shipmentList(int size, int page);
-    public Page<ShipmentResponse> shipmentList(int size, int page, String keyword, int category);
+    public Page<ShipmentResponse> shipmentList(int size, int page, String keyword, int category, LocalDateTime startDate, LocalDateTime endDate);
     public List<Code> findByCodeCategory(String codeCategory);
     public ShipmentDetailResponse findByShimentId(int shipmentId);
 
